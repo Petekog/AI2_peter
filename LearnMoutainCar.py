4485226cb4f7208b30885ce89214bd057d422368
zimgalia <zimgalia@gmail.com>
2020-01-06 14:02:16 +0200

    Add some comments

--- LearnMoutainCar.py
+++ LearnMoutainCar.py
@@ -21,6 +21,7 @@ class Tiling:
         self.bin_sizes = bin_sizes
         self.lows = lows + offsets
         self.highs = highs + offsets
+        # holds the size of the bins per dim
         self.bin_nums = np.zeros(Tiling.dim, dtype=np.int8)
         for index in range(Tiling.dim):
             self.bin_nums[index] = int(np.ceil((self.highs[index] - self.lows[index]) / self.bin_sizes[index]))
@@ -28,7 +29,7 @@ class Tiling:
         self.number_of_tiles = ACTION_NUM * self.number_of_tiles_per_action
 
     def get_tile_feature(self, state, action):
-        # bins start from 0
+        # Returns a feature vector of zeroes except the state,action feature which is 1
         tile = self.get_tile_number(state)
         action_tile = action * self.number_of_tiles_per_action + tile
         features = np.zeros(self.number_of_tiles)
@@ -36,13 +37,14 @@ class Tiling:
         return features
 
     def get_tile_number(self, state):
+        # Returns the tile number of the state according to its 2 dimensions
         bins = []
         for low, bin_size, s in zip(self.lows, self.bin_sizes, state):
             bins.append(int(np.floor((s - low) / bin_size)))
         tile = bins[0] + (bins[1] * self.bin_nums[0])
         return tile
 
-    # return vector size 3 of vectors. every vector of size number of tiles*3
+    # return vector size 3 of vectors. every vector of size number_of_tiles_per_action*3
     def get_features_per_action_for_state(self, state):
         tile = self.get_tile_number(state)
         features_per_action = [[], [], []]
@@ -91,7 +93,6 @@ class EGreedyApproximatedValue:
         self.epsilon_denominator = 1
 
     def pick_greedy_action_and_get_value_and_features(self, state):
-        # if there is more than one best action, chooses randomly from the best actions
         actions_features = self.approximator.get_features_per_action_for_state(state)
         action_values = []
         for action_features in actions_features:
@@ -116,11 +117,11 @@ class EGreedyApproximatedValue:
             return self.pick_greedy_action_and_get_value_and_features(state)
 
     def update_weights(self, funct):
-        # funct argument recieves (Q(s,a) , E(s,a))
+        # funct argument receives (Q(s,a) , E(s,a))
         self.W = funct(self.W, self.E)
 
     def update_Es(self, funct):
-        # funct argument recieves (E(s,a))
+        # funct argument receives (E(s,a))
         self.E = funct(self.E)
 
     def reset_E(self):
@@ -146,15 +147,15 @@ class SarsaLambda:
                 # observation = observation_next
                 action_value = action_next_value
 
-            # Get R_t+1, O_t+1, A_t+1 and Q(O_t+1,A_t+1)
+            # Get R_t+1, O_t+1, A_t+1 and x(O_t+1,A_t+1)
             observation_next, reward, done, _ = env.step(action)
             action_next, action_next_value, features = self.policy.pick_action_and_get_value_and_features(observation_next)
 
-            delta = reward + GAMMA * action_next_value - action_value
-            alpha = 1 / math.log(steps_num + step + 2)
             steps_num += 1
 
-            # Update policy
+            # Update policy according to TD(lambda) formula
+            delta = reward + GAMMA * action_next_value - action_value
+            alpha = 1 / math.log(steps_num + step + 2)
             self.policy.update_weights(lambda w, e: w + alpha * delta * e)
             self.policy.update_Es(lambda e: GAMMA * LAMBDA * e + features)
 
@@ -202,6 +203,6 @@ def plot_errorbar(intervals, returns):
 
 
 if __name__ == '__main__':
-    env = gym.make('Taxi-v3')
+    env = gym.make('MountainCar-v0')
     main_loop()
     env.close()
